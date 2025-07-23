4a23754128b6194c390662a251970143c0e3d50e
Rishikesh <kesm3052021@gmail.com>
2025-07-24 04:10:48 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import React from "react";
 import Timeline from "./components/Timeline";
 
-export default function App() {
+function App() {
   return (
-    <div className="bg-white text-blue-600 min-h-screen p-4">
-      <h1 className="text-3xl font-bold text-center mb-6">CARE Foundation Timeline</h1>
+    <div>
       <Timeline />
     </div>
   );
 }
+
+export default App;
